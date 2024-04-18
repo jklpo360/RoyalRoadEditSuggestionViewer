bfba666ec289ae59bd96463bb596a3ee35cb366a
EDScodes <44623646+jklpo360@users.noreply.github.com>
2024-04-18 05:36:31 -0500

    initial css and html coming together - trying to do a dry run and then look into web design libraries

--- src/index2.js
+++ src/index2.js
@@ -1,11 +1,16 @@
 const http = require("http");
+const fs = require('fs').promises
 
 const host = 'localhost';
 const port = 8000;
 
 const requestListener = function (req, res) {
-    res.writeHead(200);
-    res.end("My first server!");
+    fs.readFile(__dirname + "/index.html")
+        .then(contents => {
+            res.setHeader("Content-Type", "text/html");
+            res.writeHead(200);
+            res.end(contents);
+        })
 };
 
 const server = http.createServer(requestListener);
